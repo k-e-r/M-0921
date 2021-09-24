f94b3aaf1c801a632cc8871fb8e3812befe2a9e2
k-e-r <damajonboua@gmail.com>
2021-09-24 15:45:41 -0700

    add class' solution

--- 0923_codingtask.js
+++ 0923_codingtask.js
@@ -63,6 +63,8 @@ console.log("----------------------");
 
 // solution3(ex.the shortest code)
 // compare current value with next value
+// prerequisite
+// array and not same values
 function isSorted3(arr) {
   let j = 0;
   for (i = 0; i < arr.length - 1; i++) arr[i] < arr[i + 1] ? j++ : j--;
@@ -77,3 +79,35 @@ console.log(isSorted3([42, 39]));
 console.log(isSorted3([1, 2, 3, 4, 5]));
 console.log(isSorted3([5, 4, 3, 2, 1]));
 console.log(isSorted3([1, 5, 2]));
+
+console.log("----------------------");
+
+// class' answer
+function isSorted(arr) {
+  let isDescending = true;
+  let isAscending = true;
+
+  for (let i = 1; i < arr.length; i++) {
+    let v1 = arr[i - 1];
+    let v2 = arr[i];
+
+    if (v1 < v2) {
+      isDescending = false;
+    }
+
+    if (v1 > v2) {
+      isAscending = false;
+    }
+  }
+
+  return isAscending || isDescending;
+}
+
+console.log("Class' Solution:");
+console.log(isSorted([]));
+console.log(isSorted([42]));
+console.log(isSorted([39, 42]));
+console.log(isSorted([42, 39]));
+console.log(isSorted([1, 2, 3, 4, 5]));
+console.log(isSorted([5, 4, 3, 2, 1]));
+console.log(isSorted([1, 5, 2]));
