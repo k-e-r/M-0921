ce8ed41dec1b6e097870a19c81885d9197a189fb
k-e-r <damajonboua@gmail.com>
2021-10-04 22:39:09 -0700

    add Java sample

---
+++ Java/JavaProject/src/sample.java
@@ -0,0 +1,7 @@
+public class sample {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int value1 = 1;
+        System.out.println(value1);
+    }
+}
